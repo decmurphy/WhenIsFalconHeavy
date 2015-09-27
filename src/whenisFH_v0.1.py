3b6f0501d2c23cec602e55e94f0b70dc2c5a7097
Declan <murphd37@tcd.ie>
2015-09-27 10:56:57 -0400

    Adding db access, removing codeswq

--- src/whenisFH_v0.1.py
+++ src/whenisFH_v0.1.py
@@ -1,6 +1,9 @@
 import praw
 import time
 import datetime
+import MySQLdb
+import dbprops
+import ConfigParser
 
 from dateutil.relativedelta import relativedelta
 
@@ -8,12 +11,20 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-CLIENT_ID = 'My4z-cA89mwPAA'
-CLIENT_SECRET = 'S75z9JF0CKdLQUhyZvHBKUpfDto'
-REDIRECT_URI = 'http://flask.decmurphy.com/authorize_callback'
+Config = ConfigParser.ConfigParser()
+Config.read('praw.ini')
+CLIENT_ID = Config.get('test','client_id')
+CLIENT_SECRET = Config.get('test','client_secret')
+REDIRECT_URI = Config.get('test','redirect_uri')
 
 access_information = ''
 
+db = MySQLdb.connect(host=dbprops.host, # your host, usually localhost
+                      port=dbprops.port,
+                      user=dbprops.user, # your username
+                      passwd=dbprops.password, # your password
+                      db=dbprops.database) # name of the data base
+                      
 @app.route('/')
 def homepage():
     link_refresh = r.get_authorize_url('DifferentUniqueKey','identity read submit', refreshable=True)
@@ -29,37 +40,46 @@ def authorized():
     access_information = r.get_access_information(code)  
     activate()
     return "Success"
-
+  
 @app.route('/activate')
 def activate():
 
     replied = []
 
-    orig_date = datetime.date(2017, 1, 1)
+    # Use all the SQL you like
+    cur = db.cursor()
+    cur.execute("""SELECT month, year FROM Date;""")
+
+    # print all the first cell of all the rows
+    row = cur.fetchone()
+    month = row[0]
+    year = row[1]
+    
+    orig_date = datetime.date(year, month, 1)
     date = orig_date
     one_month = relativedelta(months=1)
-    
-    clock = 0
-    
+        
     while True:
-        comments_by_sub = r.get_comments('test',limit=10)
+      try:
+        comments_by_sub = r.get_comments('spacex',limit=10)
         for comment in comments_by_sub:
             if comment.author is not None:
-                if 'newtrigger' in comment.body.lower() and comment.id not in replied:
+                if 'falconheavy' in comment.body.lower() and comment.id not in replied:
                     date = date + one_month
-                    new_comment = comment.reply('You mentioned Trigger. By doing so you have '
+                    executeQuery = "UPDATE Date SET Year=%d, Month=%d" % (date.year, date.month)
+                    cur.execute(executeQuery)
+                    new_comment = comment.reply('You mentioned Falcon Heavy. By doing so you have '
                         'pushed the NET date one month into the future.\n\nThe new '
-                        'NET is {month} {year}.\n\nThanks a lot.'
+                        'NET is {month} {year}.\n---\n^(I am a bot. If you have feedback, please message /u/TheVehicleDestroyer)'
                         .format(month=date.strftime("%B"), year=date.year))
                     replied.append(comment.id)
                     replied.append(new_comment.id)
         
-        time.sleep(5)
+      except praw.errors.OAuthInvalidToken:
         
-        clock = clock + 5
-        if clock > 3000:
-            r.refresh_access_information(access_information['refresh_token'])
-            clock = clock - 3000
+        print 'Refreshing token...'
+        access_information = r.refresh_access_information(access_information['refresh_token'])
+        print 'Token refreshed'
 
     return "Activation finished"
 
